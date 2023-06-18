82672770621cc4cb321957be0f956fddaccfd431
Marcos Cavalcante <54518635+MarcosRaoi@users.noreply.github.com>
2023-06-18 18:40:44 -0300

    Rendering poke ID, resolved endpoint to lower case, need to ADD gif poke img now

--- js/script.js
+++ js/script.js
@@ -3,15 +3,26 @@
 const pokeAPIEndoint = "https://pokeapi.co/api/v2/pokemon/";
 
 const pokemonNameDOM = document.querySelector(".pokemon-name");
+const pokemonIdDOM = document.querySelector(".pokemon-id");
+const pokemonImageGifDOM = document.querySelector(".pokemon-gif");
 
 function pokeNameRender(pokeDataFetched) {
-    let pokeName = pokeDataFetched.name
+    let pokeName = pokeDataFetched.name;
     pokemonNameDOM.innerHTML = pokeName;
+
+    console.log("pokeName", pokeName);
+}
+
+function pokeIdRender(pokeDataFetched) {
+    let pokeId = pokeDataFetched.id;
+    pokemonIdDOM.innerHTML = pokeId;
+
+    console.log("pokeId", pokeId);
 }
 
 const fetchPokemon = async (pokemon) => {
 
-    let pokemonURL = pokeAPIEndoint + pokemon;
+    let pokemonURL = pokeAPIEndoint + pokemon.toLowerCase();
     const pokeAPIResponse = await fetch(pokemonURL);
     const pokeData = await pokeAPIResponse.json();
 
@@ -30,7 +41,10 @@ const renderPokemon = async (pokemon) => {
     console.log("pokeDataFetched", pokeDataFetched);
 
     pokeNameRender(pokeDataFetched);
+    pokeIdRender(pokeDataFetched);
 
 }
 
-renderPokemon(1);
+// Render first pokémon <3 luv ya bulbasaurO, but char >>>>>>> is better
+// renderPokemon(1);
+renderPokemon("Bulbasaur");
