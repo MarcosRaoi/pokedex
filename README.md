866d31f5085c4a1f2f8ec798bbf1f13b6f5673c9
Marcos Cavalcante <54518635+MarcosRaoi@users.noreply.github.com>
2023-06-18 18:08:46 -0300

    First tests with PokeAPI

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # pokedex
-Pokédex with HTML, CSS and Javascript consuming PokéAPI
+Pokédex with HTML, CSS and Javascript consuming PokéAPI (https://www.youtube.com/watch?v=SjtdH3dWLa8)
--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 // Using PokeAPI (https://pokeapi.co/)
 
-const fetchPokemon = () => {
+const pokeAPIEndoint = "https://pokeapi.co/api/v2/pokemon/";
+
+const fetchPokemon = async (pokemon) => {
+
+    let pokemonURL = pokeAPIEndoint + pokemon;
+    const pokeAPIResponse = await fetch(pokemonURL);
+    console.log("pokemonURL", pokemonURL);
+    console.log("pokeAPIResponse", pokeAPIResponse);
 
 }
+
+fetchPokemon(222);
